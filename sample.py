20a5bf19aeaaa95302d1d15a500409b700f24a56
Archana <archanaanilkumar406@gmail.com>
2025-06-12 09:53:54 +0530

    initial commit

---
+++ sample.py
@@ -0,0 +1,31 @@
+#a=10
+#b=20
+#swapping methods
+
+#swapping1
+#c=a
+#a=b
+#b=c
+
+#swapping2
+#a,b=b,a
+
+#swapping3
+#b=a+b
+#a=b-a
+#b=b-a
+#print(a)
+#print(b)
+
+
+#printing method
+#print("a=",a,"b=",b,)
+#print("a={} b={}".format(a,b))
+#print(f"a={a} b={b}")
+
+#datatype identifying
+#a=10
+#print(type(a))
+
+
+
