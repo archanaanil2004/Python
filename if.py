b512ad5f801c10fe2411c2ba50e20ea5b82a3f90
Archana <archanaanilkumar406@gmail.com>
2025-06-17 09:30:27 +0530

    initial stage

--- if.py
+++ if.py
@@ -3,3 +3,11 @@
     #print ("a is greater")
 
 
+i=20
+if(i<15):
+    print("i is smaller than 15")
+    print("i'm in if block")
+else:
+    print("is is greater than 15")   
+    print("i'm in else block") 
+print("i'm not in if and not in else block")    
