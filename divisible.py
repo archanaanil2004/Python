e34b3e173b296df3978ed0234babd1b1bcc53f5b
Archana <archanaanilkumar406@gmail.com>
2025-06-17 22:36:40 +0530

    divisible

--- divisible.py
+++ divisible.py
@@ -1,5 +1,4 @@
 num = int(input("enter a number:"))
 if num % 5==0:
     print("the number is divisible by 5")
-else:
-    print("the number is not divisible by 5")    
+print("the number is not divisible by 5")    
