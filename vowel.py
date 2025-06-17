d90c13c682f8da1544d380d3b50ec6e625517033
Archana <archanaanilkumar406@gmail.com>
2025-06-17 22:38:07 +0530

    vowel.py

--- vowel.py
+++ vowel.py
@@ -1,5 +1,4 @@
 char = input("enter a character:").lower()
 if char in['a','e','i','o','u']:
     print("the character is a vowel")
-else:
-    print("the character is not a vowel")
+print("the character is not a vowel")
