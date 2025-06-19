964a44d456e601f3f6076f0ee89c51f3713007e4
Archana <archanaanilkumar406@gmail.com>
2025-06-19 22:40:06 +0530

    sum

---
+++ sum.py
@@ -0,0 +1,9 @@
+num = int(input("Enter a number: "))
+sum  = 0
+temp = num
+
+while temp > 0:
+    digit = temp % 10      # Get the last digit
+    sum += digit    # Add it to sum
+    temp //= 10 
+print("the sum=",sum)     # Remove the last digit
