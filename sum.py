79496c14fa1b9e2df2f2c4581b6759529dfb9a00
Archana <archanaanilkumar406@gmail.com>
2025-06-19 22:52:46 +0530

    sum

--- sum.py
+++ sum.py
@@ -6,4 +6,4 @@ while temp > 0:
     digit = temp % 10      # Get the last digit
     sum += digit    # Add it to sum
     temp //= 10 
-print("the sum=",sum)     # Remove the last digit
+print("the sum=",sum)     
