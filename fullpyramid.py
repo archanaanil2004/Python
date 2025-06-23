5daf7a6f028f4f8bd5dd81cae66b9aa6641c82dc
Archana <archanaanilkumar406@gmail.com>
2025-06-23 10:32:08 +0530

    pattern

---
+++ fullpyramid.py
@@ -0,0 +1,8 @@
+n=5
+for i in range(0,n):
+    for j in range(0,n-i):
+      print(" ",end=" ") 
+    for k in range(0,i+1):
+       print(" * ",end="")
+    print()
+    
--- stringrev.py
+++ stringrev.py
@@ -3,3 +3,6 @@ reverse=""
 for i in range(len(text)):
     reverse =text[i]+reverse
 print("reversed string:",reverse)
+
+# a=text[::-1]
+#print(a)
