557acc11d40c2f63f97dfd170b8b88a46253721e
Archana <archanaanilkumar406@gmail.com>
2025-08-05 09:01:22 +0530

    prgrm

---
+++ module.py
@@ -0,0 +1,23 @@
+# import time
+# print(time.asctime(time.localtime(time.time())))
+
+# import time 
+# for i in range(0,5):
+#     print(i)
+#     time.sleep(5)
+# print("hy")
+
+
+
+# import datetime
+# print(datetime.datetime.now())
+
+
+
+# import calendar
+# cal=calendar.month(2020,3)
+# print(cal)
+
+# import calendar
+# cal=calendar.calendar(2020)
+# print(cal)
---
+++ regex.py
@@ -0,0 +1,27 @@
+#1)//MATCH
+ 
+# # import re
+# a="Hello World"
+# b=re.match("Hello",a)
+# print(b)
+
+#2)//SEARCH
+
+# import re
+# a="my fav number is 5,number"
+# b=re.search("number",a)
+# print(b)
+
+#3)//SPLIT
+
+# import re 
+# a="my name,is anu"
+# b=re.split("name",a)
+# print(b)
+
+#4)//SUB
+
+# import re 
+# a="my name is anu,my age is 20"
+# b=re.sub("anu","ammu",a)
+# print(b)
